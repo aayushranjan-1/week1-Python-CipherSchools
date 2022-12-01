c6d87417dc07f68dec5df2df31fd5295d331b876
Ayush Ranjan <115418486+aayushranjan-1@users.noreply.github.com>
2022-12-01 23:41:33 +0530

    Add files via upload

---
+++ class3-Print Command Functionalities-CipherSchools.py
@@ -0,0 +1,17 @@
+@@ -0,0 +1,17 @@
+#knowing more about print
+
+print("ashish kumar")
+print(1)
+
+
+#print can accept multiple values at once
+print(1, 2, 3, "ashish", 3.4, 1 + 3j)
+
+
+print("hello", end=";")
+print("world")
+
+
+
+print(1, 2, 3, "ashish", 3.4, 1 + 3j, sep=",")
